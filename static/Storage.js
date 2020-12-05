7dffbeec6ebd7dd2b86f61a89efa90082d512eaa
Thomas Gibson <thomasmgibson@gmail.com>
2020-12-05 14:47:04 -0500

    First Push

---
+++ static/Storage.js
@@ -0,0 +1,95 @@
+function cartsend2() {
+	var data = []
+	var i;
+	console.log("local storage");
+	for (i = 0; i < localStorage.length; i++)   {
+
+		let key = localStorage.key(i);
+		let value = localStorage.getItem(key);
+		let next = {};
+		next[key] = value;
+		data.push(next);
+
+	}
+
+	document.getElementById("SneakValue").value = JSON.stringify(data);
+	document.forms["List2"].submit();
+}
+
+function SaveItem() {
+			
+	var name = document.forms.ShoppingList.name.value;
+	var data = document.forms.ShoppingList.data.value;
+	var obj = {Product: name, Amount: data};
+	localStorage.setItem(obj.Product,obj.Amount);
+	console.log(obj); 
+	doShowAll();
+	
+}
+//change an existing key=>value in the HTML5 storage
+function ModifyItem() {
+	var name1 = document.forms.ShoppingList.name.value;
+	var data1 = document.forms.ShoppingList.data.value;
+	//check if name1 is already exists
+	
+//check if key exists
+			if (localStorage.getItem(name1) !=null)
+			{
+			  //update
+			  localStorage.setItem(name1,data1);
+			  document.forms.ShoppingList.data.value = localStorage.getItem(name1);
+			}
+		
+	
+	doShowAll();
+}
+//-------------------------------------------------------------------------
+//delete an existing key=>value from the HTML5 storage
+function RemoveItem() {
+	var name = document.forms.ShoppingList.name.value;
+	document.forms.ShoppingList.data.value = localStorage.removeItem(name);
+	doShowAll();
+}
+//-------------------------------------------------------------------------------------
+//restart the local storage
+function ClearAll() {
+	localStorage.clear();
+	doShowAll();
+}
+
+
+//--------------------------------------------------------------------------------------
+// dynamically populate the table with shopping list items
+//below step can be done via PHP and AJAX too. 
+function doShowAll() {
+	if (CheckBrowser()) {
+		var key = "";
+		var list = "<tr><th>Item</th><th>Value</th></tr>\n";
+		var i = 0;
+		//for more advance feature, you can set cap on max items in the cart
+		for (i = 0; i <= localStorage.length-1; i++) {
+			key = localStorage.key(i);
+			list += "<tr><td>" + key + "</td>\n<td>"
+					+ localStorage.getItem(key) + "</td></tr>\n";
+		}
+		//if no item exists in the cart
+		if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+			list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+		}
+		//bind the data to html table
+		//you can use jQuery too....
+		document.getElementById('list').innerHTML = list;
+	} else {
+		alert('Cannot save shopping list as your browser does not support HTML 5');
+	}
+}
+
+function CheckBrowser() {
+	if ('localStorage' in window && window['localStorage'] !== null) {
+		// we can use localStorage object to store data
+		return true;
+	} else {
+			return false;
+	}
+}
+
---
+++ templates/README.md
@@ -0,0 +1,6 @@
+An Html forum used as a ui to send a tally of products to be run through
+the beesbutter python inventory.
+
+To Do:
+Getting lost in the use of local storage for the shopping cart meeting up with php files. 
+The goal is to get the submitted data to an api my python inventory can access.
